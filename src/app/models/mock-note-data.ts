3acbd74f6507d805fc4111a7e2bb91a365895521
jaykhan-dev <jaykhan.sound@gmail.com>
2023-05-03 19:20:26 -0400

    added notes design

---
+++ src/app/models/mock-note-data.ts
@@ -0,0 +1,23 @@
+export const mockNoteData = [
+  {
+    id: 1,
+    title: 'Artist 1',
+    description: 'From Montreal',
+    tags: [{ name: 'alternative' }, { name: 'blues' }],
+    image: '../../assets/images/maeve.jpg',
+  },
+  {
+    id: 2,
+    title: 'Artist 2',
+    description: 'From New York',
+    tags: [{ name: 'electronic' }, { name: 'folk' }],
+    image: '../../assets/images/maeve.jpg',
+  },
+  {
+    id: 3,
+    title: 'Artist 3',
+    description: 'From London',
+    tags: [{ name: 'hip-hop' }],
+    image: '../../assets/images/maeve.jpg',
+  },
+];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Meta } from '@angular/platform-browser';
+import { Note } from 'src/app/models/note';
+import { mockNoteData } from 'src/app/models/mock-note-data';
 
 @Component({
   selector: 'app-home',
@@ -10,8 +12,11 @@ export class HomeComponent {
   title: string = 'Blue Notes';
   description: string = 'A music web client built on the NOSTR protocol';
 
+  notes: Note[] = [];
+
   constructor(private meta: Meta) {
     this.meta.addTag({ name: 'title', content: this.title });
     this.meta.addTag({ name: 'description', content: this.description });
+    this.notes = mockNoteData;
   }
 }
