14991f4025d5ad81727843a9f37bac702d88c5d5
jaykhan-dev <jaykhan.sound@gmail.com>
2023-05-03 11:00:48 -0400

    added common tags

--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -3,6 +3,7 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { Links } from 'src/app/models/links';
+import { Tags } from 'src/app/models/tags';
 
 @Component({
   selector: 'app-main-nav',
@@ -12,10 +13,32 @@ import { Links } from 'src/app/models/links';
 export class MainNavComponent {
   toggle: boolean = false;
 
-  links: Links[] = [
-    { path: '/', icon: 'home', title: 'Home' },
-    { path: '/about', icon: 'info', title: 'About' },
-    { path: '/contact', icon: 'email', title: 'Contact' },
+  links: Links[] = [];
+
+  // Tags that are commonly used to filter music related content
+  // can be removed
+  tags: Tags[] = [
+    { name: 'alternative' },
+    { name: 'blues' },
+    { name: 'classical' },
+    { name: 'country' },
+    { name: 'dance' },
+    { name: 'electronic' },
+    { name: 'folk' },
+    { name: 'hip-hop' },
+    { name: 'indie' },
+    { name: 'jazz' },
+    { name: 'latin' },
+    { name: 'metal' },
+    { name: 'pop' },
+    { name: 'punk' },
+    { name: 'r&b' },
+    { name: 'rap' },
+    { name: 'reggae' },
+    { name: 'rock' },
+    { name: 'soul' },
+    { name: 'soundtrack' },
+    { name: 'world' },
   ];
 
   isHandset$: Observable<boolean> = this.breakpointObserver
@@ -25,7 +48,13 @@ export class MainNavComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.links = [
+      { path: '/', icon: 'home', title: 'Home' },
+      { path: '/about', icon: 'info', title: 'About' },
+      { path: '/contact', icon: 'email', title: 'Contact' },
+    ];
+  }
 
   toggleMenu() {
     this.toggle = !this.toggle;
